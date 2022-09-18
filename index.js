d648868c96d3e6f111782c6f9e1a60df82fe290b
Umair1605 <umair1605b@gmail.com>
2022-09-18 23:58:59 +0500

    Deploying

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`)
 })
